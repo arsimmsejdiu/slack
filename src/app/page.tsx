2aa13f78a7543b26e99a20a21eea3339836841bf
arsimsejdiu <arsim.sejdiu@bik-air.com>
2024-08-28 11:13:54 +0200

    Finished designing the login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import { AuthScreen } from "@/features/auth/_components/auth-screen";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <AuthScreen />
-    </main>
   );
 }
---
+++ src/features/auth/_components/sign-up-card.tsx
@@ -0,0 +1,7 @@
+export const SignUpCard = () => {
+    return (
+        <div>
+            Sing up Card
+        </div>
+    )
+}
